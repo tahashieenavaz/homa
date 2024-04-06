96ddc0af7fd9507fbde8f31ac2b7e53a9652e552
tahashieenavaz <tahashieenavaz@gmail.com>
2024-04-06 13:52:06 +0200

    Removes unnecessary functions

--- homa/main.py
+++ homa/main.py
@@ -13,43 +13,6 @@ def destroy(key: str | None = None) -> None:
     cv2.destroyAllWindows()
 
 
-def win(key: str):
-    cv2.namedWindow(key)
-
-
-def path(directory: str) -> None:
-    Repository.directory = directory
-
-
-def write(key: str, filename: str) -> None:
-    cv2.imwrite(
-        filename=filename,
-        img=Repository.images[key]
-    )
-
-
-def save(*args, **kwargs) -> None:
-    write(args, kwargs)
-
-
-def image(filename: str, key: str | None = None, color: bool = True) -> None:
-    # TODO: add no extension in the file
-    if key is None:
-        key = filename.split(".")[0]
-
-    Repository.images[key] = cv2.imread(filename, int(color))
-    return Repository.images[key]
-
-
-def wait(delay=0):
-    cv2.waitKey(delay)
-
-
-def showWait(*args, **kwargs):
-    kwargs["wait"] = True
-    show(*args, **kwargs)
-
-
 def show(*windows, **settings):
     for window in windows:
         window.show()
@@ -58,18 +21,9 @@ def show(*windows, **settings):
         cv2.waitKey()
 
 
-def black(key: str, width: int, height: int, channels=3):
-    repo(
-        key,
-        numpy.zeros([height, width, channels], dtype="uint8")
-    )
-
-
-def white(key: str, width: int, height: int, channels=3):
-    repo(
-        key,
-        numpy.ones([height, width, channels], dtype="uint8") * (2 ** 8 - 1)
-    )
+def showWait(*args, **kwargs):
+    kwargs["wait"] = True
+    show(*args, **kwargs)
 
 
 def setting(key: str, value: any = None) -> any:
@@ -79,15 +33,3 @@ def setting(key: str, value: any = None) -> any:
 
     setting_value = Repository.settings[key]
     return setting_value if setting_value else None
-
-
-def refresh(key: str) -> None:
-    cv2.imshow(Repository.windows[key], Repository.images[key])
-
-
-def equipWithRefresh(function: callable) -> callable:
-    def inner(*args, **kwargs):
-        function(*args, **kwargs)
-        refresh(args[0])
-
-    return inner
