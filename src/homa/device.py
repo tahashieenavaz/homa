a7c9c42ee35b8e78a97b0b01bc81648b961e295e
tahashieenavaz <tahashieenavaz@gmail.com>
2025-10-25 16:51:47 +0200

    chore: implements cuda, cpu, mps functions

---
+++ src/homa/device.py
@@ -0,0 +1,25 @@
+import torch
+
+
+def get_device():
+    if torch.backends.mps.is_available():
+        return mps()
+    if torch.cuda.is_available():
+        return cuda()
+    return cpu()
+
+
+def cpu():
+    return torch.device("cpu")
+
+
+def cuda():
+    return torch.device("cuda")
+
+
+def mps():
+    return torch.device("mps")
+
+
+def device():
+    return get_device()
