41704b70f3cd863a75f3fbfa8cd105bb285771b4
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-02 11:30:54 +0100

    moves actions back to the root of activation sub module

--- src/homa/activations/__init__.py
+++ src/homa/activations/__init__.py
@@ -1,1 +1,5 @@
-from .learnable import *
+from .APLU import APLU
+from .MELU import MELU
+from .SReLU import SReLU
+from .GALU import GALU
+from .SmallGALU import SmallGALU
--- src/homa/activations/learnable/__init__.py
+++
@@ -1,1 +0,0 @@
-from .APLU import APLU
