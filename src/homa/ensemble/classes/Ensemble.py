29ef5d1d73afa7e5e7e4563d8d921ffc3e0ad597
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-02 19:46:30 +0100

    adds ensemble class base

---
+++ src/homa/ensemble/classes/Ensemble.py
@@ -0,0 +1,32 @@
+import torch
+
+
+class Ensemble:
+    def __init__(self, num_classes: int):
+        super().__init__()
+        self.num_classes = num_classes
+        self.models: list = []
+
+    @property
+    def size(self):
+        return len(self.models)
+
+    @property
+    def length(self):
+        return len(self.models)
+
+    def record(self, model: torch.nn.Module):
+        self.models.append(model)
+
+    def push(self, *args, **kwargs):
+        self.record(*args, **kwargs)
+
+    def add(self, *args, **kwargs):
+        self.record(*args, **kwargs)
+
+    def logits(self, x: torch.Tensor) -> torch.Tensor:
+        batch_size = x.shape[0]
+        logits = torch.zeros((batch_size, self.num_classes))
+        for model in self.models:
+            logits += model(x)
+        return logits
