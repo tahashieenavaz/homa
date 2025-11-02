fd23532a557c277b37b83bd6269073acd845db71
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-02 12:44:30 +0100

    finishes stochastic resnet implementation

--- src/homa/models/StochasticResnet.py
+++ src/homa/models/StochasticResnet.py
@@ -1,18 +1,21 @@
 import torch
 from torchvision.models import resnet50
-from torchvision.models import ResNet50_Weights
 from .utils import replace_relu
 
 
 class StochasticResnet(torch.nn.Module):
     def __init__(self, outputs: int):
         super(StochasticResnet, self).__init__()
-        self.encoder = resnet50(ResNet50_Weights)
-        self.encoder.fc = torch.nn.Identity()
+        self._create_encoder()
+        self._create_activation_pool()
         self.fc = torch.nn.Linear(2048, outputs)
         replace_relu(self.resnet, self.activation_pool)
 
-    def create_activation_pool(self):
+    def _create_encoder(self):
+        self.encoder = resnet50(weights="DEFAULT")
+        self.encoder.fc = torch.nn.Identity()
+
+    def _create_activation_pool(self):
         self.activation_pool = [torch.nn.ReLU()]
 
     def forward(self, images: torch.Tensor):
