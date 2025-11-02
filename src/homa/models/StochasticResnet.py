792da6283827a3acbafea5c694da0265710ec63a
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-02 17:53:36 +0100

    make StochasticResnet compatible with new RandomActivation

--- src/homa/models/StochasticResnet.py
+++ src/homa/models/StochasticResnet.py
@@ -1,33 +1,19 @@
 import torch
 from torchvision.models import resnet50
 from .utils import replace_relu
-from ..activation import APLU, MELU, WideMELU, GALU, SmallGALU
+from ..activation import RandomActivation
 
 
 class StochasticResnet(torch.nn.Module):
     def __init__(self, outputs: int):
         super(StochasticResnet, self).__init__()
         self._create_encoder()
-        self._create_activation_pool()
         self.fc = torch.nn.Linear(2048, outputs)
-        replace_relu(self.resnet, self.activation_pool)
+        replace_relu(self.resnet, RandomActivation)
 
     def _create_encoder(self):
         self.encoder = resnet50(weights="DEFAULT")
         self.encoder.fc = torch.nn.Identity()
-
-    def _create_activation_pool(self):
-        self.activation_pool = [
-            torch.nn.ReLU(),
-            torch.nn.LeakyReLU(),
-            torch.nn.PReLU(),
-            torch.nn.ELU(),
-            APLU(),
-            MELU(),
-            WideMELU(),
-            GALU(),
-            SmallGALU(),
-        ]
 
     def forward(self, images: torch.Tensor):
         features = self.encoder(images)
