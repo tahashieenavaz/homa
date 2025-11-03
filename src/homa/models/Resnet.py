06a0808847465741603a04f26c05cfa516640338
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-03 11:46:42 +0100

    corrects the typo

--- src/homa/models/Resnet.py
+++ src/homa/models/Resnet.py
@@ -7,7 +7,7 @@ class Resnet(Model):
     def __init__(self, num_classes: int, lr: float):
         super().__init__()
         self.network = ResnetModule(num_classes)
-        self.optimizer = torch.optim.SGD(self.model.parameters(), lr=lr, momentum=0.9)
+        self.optimizer = torch.optim.SGD(self.network.parameters(), lr=lr, momentum=0.9)
         self.criterion = torch.nn.CrossEntropyLoss()
 
     def train(self, x: torch.Tensor, y: torch.Tensor):
