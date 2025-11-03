2130378eb035d6daf14128ed73b9c4064090a35b
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-03 15:52:21 +0100

    moves resnet to the dominant device

--- src/homa/vision/Resnet.py
+++ src/homa/vision/Resnet.py
@@ -2,11 +2,12 @@ import torch
 from .modules import ResnetModule
 from .Model import Model
 from .concerns import Trainable
+from ..device import get_device
 
 
 class Resnet(Model, Trainable):
     def __init__(self, num_classes: int, lr: float):
         super().__init__()
-        self.network = ResnetModule(num_classes)
+        self.network = ResnetModule(num_classes).to(get_device())
         self.criterion = torch.nn.CrossEntropyLoss()
         self.optimizer = torch.optim.SGD(self.network.parameters(), lr=lr, momentum=0.9)
