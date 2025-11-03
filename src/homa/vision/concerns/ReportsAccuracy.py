99c511ab1a66d899ea45af228aaa289ea64c67bd
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-03 16:34:44 +0100

    fixes reports accuracy device problem

--- src/homa/vision/concerns/ReportsAccuracy.py
+++ src/homa/vision/concerns/ReportsAccuracy.py
@@ -1,24 +1,27 @@
 from torch import Tensor, no_grad
 from torch.utils.data.dataloader import DataLoader
+from ...device import get_device
 
 
 class ReportsAccuracy:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+    @no_grad()
     def accuracy_tensors(self, x: Tensor, y: Tensor) -> float:
         predictions = self.predict(x)
         return (predictions == y).float().mean().item()
 
+    @no_grad()
     def accuracy_dataloader(self, dataloader: DataLoader):
         correct, total = 0, 0
         for x, y in dataloader:
+            x, y = x.to(get_device()), y.to(get_device())
             predictions = self.predict(x)
             correct += (predictions == y).sum().item()
             total += y.numel()
         return correct / total if total > 0 else 0.0
 
-    @no_grad()
     def accuracy(self, x: Tensor | DataLoader, y: Tensor | None = None) -> float:
         self.network.eval()
         if isinstance(x, DataLoader):
