cd10a6925e88836aee0541579fd9f14b95d1806b
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-04 11:43:36 +0100

    makes Swin trainable and report metrics

--- src/homa/vision/Swin.py
+++ src/homa/vision/Swin.py
@@ -1,9 +1,10 @@
 import torch
 from .ClassificationModel import ClassificationModel
+from .concerns import Trainable, ReportsMetrics
 from .modules import SwinModule
 
 
-class Swin(ClassificationModel):
+class Swin(ClassificationModel, Trainable, ReportsMetrics):
     def __init__(self, num_classes: int, lr: float = 0.0001):
         super().__init__()
         self.network = SwinModule(num_classes=num_classes)
