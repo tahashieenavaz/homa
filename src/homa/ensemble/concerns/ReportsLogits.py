0618acfd277350452c8a95e2639cdbdd6e72932a
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-04 09:32:34 +0100

    adds logits_ for logits without gradients

--- src/homa/ensemble/concerns/ReportsLogits.py
+++ src/homa/ensemble/concerns/ReportsLogits.py
@@ -13,6 +13,11 @@ class ReportsLogits:
         self.panic_if_no_network()
         batch_size = x.shape[0]
         logits = torch.zeros((batch_size, self.num_classes))
-        for model in self.models:
-            logits += model(x)
+        for state_dict in self.state_dicts:
+            self.network.load_state_dict(state_dict)
+            logits += self.network(x)
         return logits
+
+    @torch.no_grad()
+    def logits_(self, x: torch.Tensor) -> torch.Tensor:
+        return self.logits(x)
