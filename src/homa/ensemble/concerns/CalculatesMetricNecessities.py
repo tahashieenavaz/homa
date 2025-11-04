0a392b820d3c71b7cdcdd4df1430f43490da5f27
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-04 10:36:59 +0100

    inlines the sum of logits

--- src/homa/ensemble/concerns/CalculatesMetricNecessities.py
+++ src/homa/ensemble/concerns/CalculatesMetricNecessities.py
@@ -1,20 +1,26 @@
 import torch
+from ...device import get_device
 
 
 class CalculatesMetricNecessities:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+    @torch.no_grad()
     def metric_necessities(self, dataloader):
-        all_predictions = []
-        all_labels = []
+        predictions, labels = [], []
+        device = get_device()
         for x, y in dataloader:
-            batch_logits_list = []
-            for model in self.models:
-                batch_logits_list.append(model(x))
-            all_batch_logits = torch.stack(batch_logits_list)
-            avg_logits = torch.mean(all_batch_logits, dim=0)
-            _, preds = torch.max(avg_logits, 1)
-            all_predictions.extend(preds.cpu().numpy())
-            all_labels.extend(y.cpu().numpy())
-        return all_predictions, all_labels
+            x, y = x.to(device), y.to(device)
+            sum_logits = None
+            for state_dict in self.state_dicts:
+                model = self.network.__class__()
+                model.load_state_dict(state_dict)
+                model.to(device)
+                model.eval()
+                logits = model(x)
+                sum_logits = logits if logits is None else sum_logits + logits
+            predictions = sum_logits.argmax(dim=1)
+            predictions.extend(predictions.cpu().numpy())
+            labels.extend(y.cpu().numpy())
+        return predictions, labels
