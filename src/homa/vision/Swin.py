539d740de3d6076089260be867ae1e424ad930ea
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-05 16:41:52 +0100

    fixes StochasticSwin import problem

--- src/homa/vision/Swin.py
+++ src/homa/vision/Swin.py
@@ -2,11 +2,12 @@ import torch
 from .Classifier import Classifier
 from .concerns import Trainable, ReportsMetrics
 from .modules import SwinModule
+from ..device import get_device
 
 
 class Swin(Classifier, Trainable, ReportsMetrics):
     def __init__(self, num_classes: int, lr: float = 0.0001):
         super().__init__()
-        self.network = SwinModule(num_classes=num_classes)
+        self.network = SwinModule(num_classes=num_classes).to(get_device())
         self.optimizer = torch.optim.AdamW(self.network.parameters(), lr=lr)
         self.criterion = torch.nn.CrossEntropyLoss()
