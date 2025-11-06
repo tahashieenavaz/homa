6135fd9e573d2884e3b8485ecbf73fc3ffa3ebc3
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-06 16:45:35 +0100

    removes extra line in init file

---
+++ src/homa/cli/Commands/__init__.py
@@ -0,0 +1,2 @@
+from .Command import Command
+from .InitCommand import InitCommand
--- src/homa/cli/namespaces/__init__.py
+++ src/homa/cli/namespaces/__init__.py
@@ -1,3 +1,2 @@
 from .MakeNamespace import MakeNamespace
 from .CacheNamespace import CacheNamespace
-from .InitNamespace import InitNamespace
