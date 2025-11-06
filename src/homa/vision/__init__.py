074bb06a07ae7b309a14455191269cd0b0c3d766
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-06 17:57:36 +0100

    removes extra import line

--- src/homa/vision/__init__.py
+++ src/homa/vision/__init__.py
@@ -1,6 +1,5 @@
 from .Classifier import Classifier
 from .Model import Model
 from .Resnet import Resnet
-from .StochasticResnet import StochasticResnet
 from .Swin import Swin
 from .StochasticSwin import StochasticSwin
