6ed20cce45a8491a3b56c1ae318491cf345e347e
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-07 17:48:57 +0100

    adds more configurability to Swin

--- src/homa/vision/Swin.py
+++ src/homa/vision/Swin.py
@@ -6,9 +6,20 @@ from ..core.concerns import MovesNetworkToDevice
 
 
 class Swin(Classifier, Trainable, ReportsMetrics, MovesNetworkToDevice):
-    def __init__(self, num_classes: int, lr: float = 0.0001):
+    def __init__(
+        self,
+        num_classes: int,
+        lr: float = 0.0001,
+        decay: float = 0.0,
+        variant: str = "base",
+        weights="DEFAULT",
+    ):
         super().__init__()
         self.num_classes = num_classes
-        self.network = SwinModule(num_classes=self.num_classes)
-        self.optimizer = torch.optim.AdamW(self.network.parameters(), lr=lr)
+        self.network = SwinModule(
+            num_classes=self.num_classes, variant=variant, weights=weights
+        )
+        self.optimizer = torch.optim.AdamW(
+            self.network.parameters(), lr=lr, weight_decay=decay
+        )
         self.criterion = torch.nn.CrossEntropyLoss()
--- src/homa/vision/modules/SwinModule.py
+++ src/homa/vision/modules/SwinModule.py
@@ -1,21 +1,29 @@
 import torch
-from torchvision.models import swin_v2_b
+from torchvision.models import swin_v2_b, swin_v2_s, swin_v2_t
 from torch.nn.init import kaiming_uniform_ as kaiming
 
 
 class SwinModule(torch.nn.Module):
-    def __init__(self, num_classes: int):
+    def __init__(self, num_classes: int, variant: str, weights):
         super().__init__()
-        self.num_classes = num_classes
-        self._create_encoder()
-        self._create_fc()
+        self._create_encoder(variant=variant, weights=weights)
+        self._create_fc(num_classes=num_classes)
 
-    def _create_encoder(self):
-        self.encoder = swin_v2_b(weights="DEFAULT")
+    def variant_instance(self, variant: str):
+        variant_map = {"tiny": swin_v2_t, "small": swin_v2_s, "base": swin_v2_b}
+        return variant_map.get(variant)
+
+    def _create_encoder(self, variant: str, weights):
+        if variant not in ["tiny", "small", "base"]:
+            raise ValueError(
+                f"Swin variant needs to be one of [tiny, small, base]. Invalid {variant} was provided."
+            )
+        instance = self.variant_instnace(variant)
+        self.encoder = instance(weights=weights)
         self.encoder.head = torch.nn.Identity()
 
-    def _create_fc(self):
-        self.fc = torch.nn.Linear(1024, self.num_classes)
+    def _create_fc(self, num_classes: int):
+        self.fc = torch.nn.Linear(1024, num_classes)
         kaiming(self.fc.weight, mode="fan_in", nonlinearity="relu")
 
     def forward(self, images: torch.Tensor):
