c4020e2e7b1ebf81efb8e9bba6f23d5575be403f
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-08 15:01:08 +0100

    adds soft update soft critic

--- src/homa/rl/sac/SoftCritic.py
+++ src/homa/rl/sac/SoftCritic.py
@@ -1,5 +1,6 @@
 import torch
 from .modules import DualSoftCriticModule
+from typing import Type
 
 
 class SoftCritic:
@@ -10,7 +11,10 @@ class SoftCritic:
         action_dimension: int,
         lr: float,
         weight_decay: float,
+        tau: float,
     ):
+        self.tau = tau
+
         self.network = DualSoftCriticModule(
             state_dimension=state_dimension,
             hidden_dimension=hidden_dimension,
@@ -33,3 +37,13 @@ class SoftCritic:
 
     def loss(self):
         pass
+
+    def soft_update(
+        self, network: Type[torch.nn.Module], target: Type[torch.nn.Module]
+    ):
+        for s, t in zip(network.parameters(), target.parameters()):
+            t.data.copy_(self.tau * s.data + (1 - self.tau) * t.data)
+
+    def update(self):
+        self.soft_update(self.network.alpha, self.target.alpha)
+        self.soft_update(self.network.beta, self.target.beta)
