72f04acd87cc954a10c748b9e6ce2856a5888358
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-09 19:30:41 +0100

    fixes soft_update logic

--- src/homa/rl/utils.py
+++ src/homa/rl/utils.py
@@ -2,6 +2,6 @@ import torch
 
 
 @torch.no_grad()
-def soft_update(self, network: torch.nn.Module, target: torch.nn.Module):
+def soft_update(network: torch.nn.Module, target: torch.nn.Module, tau: float):
     for s, t in zip(network.parameters(), target.parameters()):
-        t.data.copy_(self.tau * s.data + (1 - self.tau) * t.data)
+        t.data.copy_(tau * s.data + (1 - tau) * t.data)
