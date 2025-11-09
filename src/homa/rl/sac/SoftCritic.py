343098d9db46423b99165df27e249f278fe953b4
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-09 12:00:00 +0100

    adds no grad to soft update method

--- src/homa/rl/sac/SoftCritic.py
+++ src/homa/rl/sac/SoftCritic.py
@@ -92,6 +92,7 @@ class SoftCritic:
         entropy_q = q - self.alpha * next_probabilities * termination_mask
         return rewards + self.gamma * entropy_q
 
+    @torch.no_grad()
     def soft_update(self, network: torch.nn.Module, target: torch.nn.Module):
         for s, t in zip(network.parameters(), target.parameters()):
             t.data.copy_(self.tau * s.data + (1 - self.tau) * t.data)
