50e49520e820c62aee0677c75909d5421eb51edb
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-09 11:27:31 +0100

    adds collection initialization to buffer constructor

--- src/homa/rl/buffers/Buffer.py
+++ src/homa/rl/buffers/Buffer.py
@@ -1,10 +1,12 @@
+from collections import deque
+from typing import Type
 from .concerns import ResetsCollection, HasRecordAlternatives
 
 
 class Buffer(ResetsCollection, HasRecordAlternatives):
     def __init__(self, capacity: int):
         self.capacity: int = capacity
-        self.reset()
+        self.collection: Type[deque] = deque(maxlen=self.capacity)
 
     @property
     def size(self):
