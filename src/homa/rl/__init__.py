3abf590294f0081270f9132723fc578004ab3fd3
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-10 15:15:30 +0100

    updates init

--- src/homa/rl/__init__.py
+++ src/homa/rl/__init__.py
@@ -1,4 +1,6 @@
 from .DiversityIsAllYouNeed import DiversityIsAllYouNeed
 from .SoftActorCritic import SoftActorCritic
+from .TQC import TQC
 from .DQN import DQN
 from .DRQN import DRQN
+from .utils import soft_update
