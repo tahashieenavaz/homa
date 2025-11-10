c74b349da7a7af396d094d9618905913c3d8ce7a
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-10 18:55:17 +0100

    adds dataset modules

---
+++ src/homa/datasets/graph/CoraDataset.py
@@ -0,0 +1,5 @@
+from ..Dataset import Dataset
+
+
+class CoraDataset(Dataset):
+    pass
--- src/homa/graph/GraphAttention.py
+++ src/homa/graph/GraphAttention.py
@@ -4,9 +4,29 @@ from ..core.concerns import MovesNetworkToDevice
 
 
 class GraphAttention(MovesNetworkToDevice):
-    def __init__(self, lr: float = 0.005, decay: float = 5e-4, dropout: float = 0.6):
+    def __init__(
+        self,
+        input_dimension: int,
+        output_dimension: int,
+        hidden_dimension: int = 8,
+        num_heads: int = 8,
+        alpha: float = 0.2,
+        lr: float = 0.005,
+        decay: float = 0.0005,
+        dropout: float = 0.6,
+        concat: bool = True,
+    ):
         super().__init__()
-        self.network = GraphAttentionModule()
+
+        self.network = GraphAttentionModule(
+            input_dimension=input_dimension,
+            output_dimension=output_dimension,
+            hidden_dimension=hidden_dimension,
+            num_heads=num_heads,
+            dropout=dropout,
+            alpha=alpha,
+            concat=concat,
+        )
         self.optimizer = torch.nn.AdamW(
             self.network.parameters(), lr=lr, weight_decay=decay
         )
