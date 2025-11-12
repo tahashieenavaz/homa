ecae2fcf95cd918c5ff42dcb2d988159f88d3e73
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-12 14:28:10 +0100

    moves activation functions into their appropriate sub folders

--- src/homa/activations/learnable/channel_based/AOAF.py
+++ src/homa/activations/learnable/channel_based/AOAF.py
@@ -1,6 +1,6 @@
 import torch
-from .concerns import ChannelBased
-from ..AdaptiveActivationFunction import AdaptiveActivationFunction
+from ...concerns import ChannelBased
+from ...AdaptiveActivationFunction import AdaptiveActivationFunction
 
 
 class AOAF(AdaptiveActivationFunction, ChannelBased):
