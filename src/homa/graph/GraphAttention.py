878f7a166713cf4057e2f6ac0586f78ee4c6f00a
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-12 13:40:40 +0100

    moves predictions from a static property to a private one

--- src/homa/graph/GraphAttention.py
+++ src/homa/graph/GraphAttention.py
@@ -6,6 +6,9 @@ from ..common.concerns import MovesModulesToDevice
 class GraphAttention(MovesModulesToDevice):
     def __init__(
         self,
+        labels: torch.Tensor,
+        features: torch.Tensor,
+        adjacency_matrix: torch.Tensor,
         input_dimension: int,
         output_dimension: int,
         hidden_dimension: int = 8,
@@ -17,6 +20,10 @@ class GraphAttention(MovesModulesToDevice):
         concat: bool = True,
     ):
         super().__init__()
+
+        self.features = features
+        self.adjacency_matrix = adjacency_matrix
+        self.labels = labels
 
         self.network = GraphAttentionModule(
             input_dimension=input_dimension,
@@ -32,4 +39,32 @@ class GraphAttention(MovesModulesToDevice):
         self.optimizer = torch.optim.AdamW(
             self.network.parameters(), lr=lr, weight_decay=decay
         )
-        self.criterion = torch.nn.CrossEntropyLoss()
+
+        # this is used because model outputs log-probability distribution
+        self.criterion = torch.nn.NLLLoss()
+
+        # this will be used to share data between loss and accuracy methods
+        self._predictions = None
+
+    def train(self, idx: torch.Tensor):
+        self.network.train()
+        self.optimizer.zero_grad()
+        loss = self.loss(idx=idx)
+        loss.backward()
+        self.optimizer.step()
+
+    def loss(
+        self,
+        idx,
+    ):
+        self._predictions = self.network(self.features, self.adjacency_matrix)
+        masked_predictions = self._predictions[idx]
+        masked_labels = self.labels[idx]
+        return self.criterion(masked_predictions, masked_labels)
+
+    @torch.no_grad()
+    def accuracy(self, idx):
+        self.network.eval()
+        masked_predictions = self._predictions[idx].argmax(dim=1)
+        masked_labels = self.labels[idx]
+        return (masked_predictions == masked_labels).float().mean()
