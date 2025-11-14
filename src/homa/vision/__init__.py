527f542399e6edd410b288a034acc96ef9efd92f
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-14 15:15:55 +0100

    corrects init file

--- src/homa/vision/__init__.py
+++ src/homa/vision/__init__.py
@@ -1,5 +1,5 @@
 from .Classifier import Classifier
-from .VisionModel import Model
+from .VisionModel import VisionModel
 from .Resnet import Resnet
 from .Swin import Swin
 from .StochasticSwin import StochasticSwin
