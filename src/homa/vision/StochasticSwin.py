49328fb005539cd07c1f2f91637ee155bed88a68
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-14 13:57:49 +0100

    renames replace activation to replace modules

--- src/homa/vision/StochasticSwin.py
+++ src/homa/vision/StochasticSwin.py
@@ -1,11 +1,11 @@
 import torch
 from .Swin import Swin
 from .StochasticClassifier import StochasticClassifier
-from .utils import replace_activations
+from .utils import replace_modules
 
 
 class StochasticSwin(Swin, StochasticClassifier):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        replace_activations(self.network, torch.nn.GELU, self._activation_pool)
-        replace_activations(self.network, torch.nn.ReLU, self._activation_pool)
+        replace_modules(self.network, torch.nn.GELU, self._activation_pool)
+        replace_modules(self.network, torch.nn.ReLU, self._activation_pool)
--- src/homa/vision/utils.py
+++ src/homa/vision/utils.py
@@ -2,11 +2,11 @@ import torch
 import random
 
 
-def replace_activations(module, needle: torch.nn.Module, candidates: list):
+def replace_modules(module, needle: torch.nn.Module, candidates: list):
     for name, child in module.named_children():
         if isinstance(child, needle):
             new_activation = random.choice(candidates)
             setattr(module, name, new_activation())
         else:
-            replace_activations(child, needle, candidates)
+            replace_modules(child, needle, candidates)
     return module
