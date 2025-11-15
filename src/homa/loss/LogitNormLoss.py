6635481de4dc77ba228ceda47f7cc138225cf03d
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-15 13:45:17 +0100

    refines logit norm loss code

--- src/homa/loss/LogitNormLoss.py
+++ src/homa/loss/LogitNormLoss.py
@@ -3,10 +3,12 @@ from .Loss import Loss
 
 
 class LogitNormLoss(Loss):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, epsilon: float = 1e-7):
+        super().__init__()
+        self.epsilon = epsilon
 
-    def forward(self, logits, target):
-        norms = torch.norm(logits, p=2, dim=-1, keepdim=True) + 1e-7
+    def forward(self, logits: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+        norms = torch.norm(logits, p=2, dim=-1, keepdim=True)
+        norms = norms + self.epsilon
         normalized_logits = torch.div(logits, norms)
         return torch.nn.functional.cross_entropy(normalized_logits, target)
