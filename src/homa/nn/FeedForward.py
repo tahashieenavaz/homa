6e443644109dca74c520e35e1667c0d39d348cb3
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-17 14:44:48 +0100

    makes feed forward lazy

--- src/homa/nn/FeedForward.py
+++ src/homa/nn/FeedForward.py
@@ -5,15 +5,14 @@ from typing import Type
 class FeedForward(torch.nn.Module):
     def __init__(
         self,
-        input_dimension: int,
         embedding_dimension: int,
         output_dimension: int,
         activation: Type[torch.nn.Module],
     ):
         super().__init__()
 
-        self.mu = torch.nn.Linear(input_dimension, embedding_dimension)
-        self.xi = torch.nn.Linear(embedding_dimension, output_dimension)
+        self.mu = torch.nn.LazyLinear(embedding_dimension)
+        self.xi = torch.nn.LazyLinear(output_dimension)
         self.sigma = activation()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
