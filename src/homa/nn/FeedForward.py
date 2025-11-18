ffb829e20e6a7a91d2a36bd296b4af68c5863afc
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-18 14:40:15 +0100

    adds dimension factor, skip connection, and layernomr to feedforward

--- src/homa/nn/FeedForward.py
+++ src/homa/nn/FeedForward.py
@@ -5,18 +5,33 @@ from typing import Type
 class FeedForward(torch.nn.Module):
     def __init__(
         self,
-        embedding_dimension: int,
-        output_dimension: int,
-        activation: Type[torch.nn.Module],
+        dimension: int,
+        factor: int = 4,
+        activation: Type[torch.nn.Module] = torch.nn.GELU,
+        use_skip: bool = False,
+        use_layernorm: bool = False,
     ):
         super().__init__()
+        self.use_skip = use_skip
+        self.use_layernorm = use_layernorm
 
-        self.mu = torch.nn.LazyLinear(embedding_dimension)
-        self.xi = torch.nn.LazyLinear(output_dimension)
+        if use_layernorm:
+            self.chi = torch.nn.LayerNorm(dimension)
+
+        self.mu = torch.nn.Linear(dimension, dimension * factor)
+        self.xi = torch.nn.LazyLinear(dimension * factor, dimension)
         self.sigma = activation()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
+        residual = x
         x = self.mu(x)
         x = self.sigma(x)
         x = self.xi(x)
+
+        if self.use_skip:
+            x += residual
+
+        if self.use_layernorm:
+            x = self.chi(x)
+
         return x
