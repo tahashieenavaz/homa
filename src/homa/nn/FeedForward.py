a507597beb172312d2ce06545ba833a1fd18a9a9
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-18 14:41:18 +0100

    adds possibility of output dimension

--- src/homa/nn/FeedForward.py
+++ src/homa/nn/FeedForward.py
@@ -7,6 +7,7 @@ class FeedForward(torch.nn.Module):
         self,
         dimension: int,
         factor: int = 4,
+        output_dimension: int | None = None,
         activation: Type[torch.nn.Module] = torch.nn.GELU,
         use_skip: bool = False,
         use_layernorm: bool = False,
@@ -15,11 +16,14 @@ class FeedForward(torch.nn.Module):
         self.use_skip = use_skip
         self.use_layernorm = use_layernorm
 
+        if not output_dimension:
+            output_dimension = dimension
+
         if use_layernorm:
             self.chi = torch.nn.LayerNorm(dimension)
 
         self.mu = torch.nn.Linear(dimension, dimension * factor)
-        self.xi = torch.nn.LazyLinear(dimension * factor, dimension)
+        self.xi = torch.nn.Linear(dimension * factor, output_dimension)
         self.sigma = activation()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
