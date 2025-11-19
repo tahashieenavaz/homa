86ca87bbb7563d3a34f8bf903181a1e503df2555
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-19 12:23:23 +0100

    fix Patch embedding

--- src/homa/nn/PatchEmbedding.py
+++ src/homa/nn/PatchEmbedding.py
@@ -6,7 +6,7 @@ class PatchEmbedding(torch.nn.Module):
         super().__init__()
         self.patch_size: int = patch_size
         self.channels: int = channels
-        self.phi = self.Linear(channels * patch_size**2, embedding_dimension)
+        self.phi = torch.nn.Linear(channels * patch_size**2, embedding_dimension)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         batch_size, _, _, _ = x.shape
