ca795b0e85daa3b62492e8217f8f8c7fe96e73d4
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-21 13:50:11 +0100

    fixes feed forward layer

--- src/homa/nn/FeedForward.py
+++ src/homa/nn/FeedForward.py
@@ -6,13 +6,25 @@ class FeedForward(torch.nn.Module):
     def __init__(
         self,
         dimension: int,
-        factor: int = 4,
+        factor: int | None = None,
         output_dimension: int | None = None,
+        embedding_dimension: int | None = None,
         activation: Type[torch.nn.Module] = torch.nn.GELU,
         use_skip: bool = False,
         use_layernorm: bool = False,
     ):
         super().__init__()
+
+        if factor is not None and embedding_dimension is not None:
+            raise Exception(
+                "You should specify either factor or embedding dimensions in homa.nn.FeedForward. Got both."
+            )
+
+        if factor is not None and embedding_dimension is None:
+            sigma = factor * dimension
+        else:
+            sigma = embedding_dimension
+
         self.use_skip = use_skip
         self.use_layernorm = use_layernorm
 
@@ -22,8 +34,8 @@ class FeedForward(torch.nn.Module):
         if use_layernorm:
             self.chi = torch.nn.LayerNorm(dimension)
 
-        self.mu = torch.nn.Linear(dimension, dimension * factor)
-        self.xi = torch.nn.Linear(dimension * factor, output_dimension)
+        self.mu = torch.nn.Linear(dimension, sigma)
+        self.xi = torch.nn.Linear(sigma, output_dimension)
         self.sigma = activation()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
