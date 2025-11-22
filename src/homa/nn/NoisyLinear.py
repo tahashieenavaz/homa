ce562baacbae6a9f317d385a835efe121939d46a
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-22 17:28:21 +0100

    improves noisy linear

--- src/homa/nn/NoisyLinear.py
+++ src/homa/nn/NoisyLinear.py
@@ -3,10 +3,11 @@ import math
 
 
 class NoisyLinear(torch.nn.Module):
-    def __init__(self, in_features, out_features, std_init=0.5):
+    def __init__(self, in_features, out_features, std_init=0.017):
         super().__init__()
         self.in_features = in_features
         self.out_features = out_features
+        self.std_init = std_init
 
         self.weight_mu = torch.nn.Parameter(torch.empty(out_features, in_features))
         self.weight_sigma = torch.nn.Parameter(torch.empty(out_features, in_features))
@@ -16,7 +17,6 @@ class NoisyLinear(torch.nn.Module):
         self.register_buffer("eps_in", torch.empty(in_features))
         self.register_buffer("eps_out", torch.empty(out_features))
 
-        self.std_init = std_init
         self.reset_parameters()
         self.reset_noise()
 
@@ -25,22 +25,27 @@ class NoisyLinear(torch.nn.Module):
         self.weight_mu.data.uniform_(-mu_range, mu_range)
         self.bias_mu.data.uniform_(-mu_range, mu_range)
 
-        # NoisyNet recommended sigma initialization
-        self.weight_sigma.data.fill_(self.std_init / math.sqrt(self.in_features))
-        self.bias_sigma.data.fill_(self.std_init / math.sqrt(self.out_features))
+        sigma_weight = self.std_init / math.sqrt(self.in_features)
+        sigma_bias = self.std_init / math.sqrt(self.out_features)
 
-    def f(self, x):
+        self.weight_sigma.data.fill_(sigma_weight)
+        self.bias_sigma.data.fill_(sigma_bias)
+
+    @staticmethod
+    def f(x):
         return torch.sign(x) * torch.sqrt(torch.abs(x))
 
     def reset_noise(self):
         eps_in = torch.randn(self.in_features)
         eps_out = torch.randn(self.out_features)
+
         self.eps_in.copy_(self.f(eps_in))
         self.eps_out.copy_(self.f(eps_out))
 
     def forward(self, x):
         if self.training:
-            w = self.weight_mu + self.weight_sigma * (self.eps_out.ger(self.eps_in))
+            noise_w = torch.outer(self.eps_out, self.eps_in)
+            w = self.weight_mu + self.weight_sigma * noise_w
             b = self.bias_mu + self.bias_sigma * self.eps_out
         else:
             w = self.weight_mu
