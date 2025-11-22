da9ceaf78409d61631fc33e62bdc374542280445
tahashieenavaz <tahashieenavaz@gmail.com>
2025-11-22 16:16:39 +0100

    enhances noisy linear layer

--- src/homa/nn/NoisyLinear.py
+++ src/homa/nn/NoisyLinear.py
@@ -7,45 +7,44 @@ class NoisyLinear(torch.nn.Module):
         super().__init__()
         self.in_features = in_features
         self.out_features = out_features
-        self.std_init = std_init
 
-        self.weight_mu = torch.nn.Parameter(
-            torch.tensor(out_features, in_features, dtype=torch.float32)
-        )
-        self.weight_sigma = torch.nn.Parameter(torch.tensor(out_features, in_features))
-        self.register_buffer(
-            "weight_epsilon",
-            torch.tensor(out_features, in_features, dtype=torch.float32),
-        )
-        self.bias_mu = torch.nn.Parameter(
-            torch.tensor(out_features, dtype=torch.float32)
-        )
-        self.bias_sigma = torch.nn.Parameter(
-            torch.tensor(out_features, dtype=torch.float32)
-        )
-        self.register_buffer(
-            "bias_epsilon", torch.tensor(out_features, dtype=torch.float32)
-        )
+        # learned parameters
+        self.weight_mu = torch.nn.Parameter(torch.empty(out_features, in_features))
+        self.weight_sigma = torch.nn.Parameter(torch.empty(out_features, in_features))
+        self.bias_mu = torch.nn.Parameter(torch.empty(out_features))
+        self.bias_sigma = torch.nn.Parameter(torch.empty(out_features))
+
+        # factorized noise buffers
+        self.register_buffer("eps_in", torch.empty(in_features))
+        self.register_buffer("eps_out", torch.empty(out_features))
+
+        self.std_init = std_init
         self.reset_parameters()
         self.reset_noise()
 
     def reset_parameters(self):
         mu_range = 1 / math.sqrt(self.in_features)
         self.weight_mu.data.uniform_(-mu_range, mu_range)
-        self.weight_sigma.data.fill_(self.std_init / math.sqrt(self.in_features))
         self.bias_mu.data.uniform_(-mu_range, mu_range)
+
+        # NoisyNet recommended sigma initialization
+        self.weight_sigma.data.fill_(self.std_init / math.sqrt(self.in_features))
         self.bias_sigma.data.fill_(self.std_init / math.sqrt(self.out_features))
 
-    def reset_noise(self):
-        # this should be called before every training loop iteration
-        self.weight_epsilon.normal_()
-        self.bias_epsilon.normal_()
+    def f(self, x):
+        return torch.sign(x) * torch.sqrt(torch.abs(x))
 
-    def forward(self, input):
+    def reset_noise(self):
+        eps_in = torch.randn(self.in_features)
+        eps_out = torch.randn(self.out_features)
+        self.eps_in.copy_(self.f(eps_in))
+        self.eps_out.copy_(self.f(eps_out))
+
+    def forward(self, x):
         if self.training:
-            weight = self.weight_mu + self.weight_sigma * self.weight_epsilon
-            bias = self.bias_mu + self.bias_sigma * self.bias_epsilon
+            w = self.weight_mu + self.weight_sigma * (self.eps_out.ger(self.eps_in))
+            b = self.bias_mu + self.bias_sigma * self.eps_out
         else:
-            weight = self.weight_mu
-            bias = self.bias_mu
-        return torch.nn.functional.linear(input, weight, bias)
+            w = self.weight_mu
+            b = self.bias_mu
+        return torch.nn.functional.linear(x, w, b)
