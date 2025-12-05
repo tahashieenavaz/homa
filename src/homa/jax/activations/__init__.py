4f512f280cc88ea109630a2c149f7b333079ba02
tahashieenavaz <tahashieenavaz@gmail.com>
2025-12-06 00:30:21 +0100

    removes jax sub moduel

--- src/homa/jax/activations/__init__.py
+++
@@ -1,1 +0,0 @@
-from .crelu import crelu
--- src/homa/jax/activations/crelu.py
+++
@@ -1,7 +0,0 @@
-import jax
-import jax.numpy as numpy
-from jax import Array
-
-
-def crelu(x: Array) -> Array:
-    return numpy.concatenate([jax.nn.relu(x), jax.nn.relu(-x)], axis=-1)
